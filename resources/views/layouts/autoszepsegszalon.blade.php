f11c9f0a2d1d30fb4950f7deb9915ece3f8f472a
Fekete Benjámin Zoltán <feketebenjaminz@EOTVOS2.LOCAL>
2024-12-17 11:06:08 +0100

    Kéesz a layouts project létrehozasa

---
+++ resources/views/layouts/autoszepsegszalon.blade.php
@@ -0,0 +1,49 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>@yield('title')</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet">
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"></script>
+</head>
+
+<body>
+    <div class="container mt-3">
+        <ul class="nav">
+            <li class="nav-item">
+                <a class="nav-link" href="#">Link</a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link" href="#">Link</a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link" href="#">Link</a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link" href="#">Link</a>
+            </li>
+        </ul>
+    </div>
+
+    <main>
+      @yield('content')
+    </main>
+
+    <footer class="container-fluid bg-dark text-white mt-5">
+      <div class="conatiner">
+        <div class="row">
+          <div class="col-12">
+            <div class="p-5">
+              Autoszepsegszalon 2025®
+            </div>
+          </div>
+        </div>
+      </div>
+    </footer>
+
+</body>
+
+</html>
